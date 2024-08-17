31c6d160eadf8b449804b9c2c001e3f0dfb9101a
Gustavo <rosalesagustavo@gmail.com>
2024-08-17 18:50:30 -0400

    middleware updated

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -2,16 +2,15 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 // const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
 
-const isProtectedRoute = createRouteMatcher(['/', '/api/webhooks(.*)']);
+// const isProtectedRoute = createRouteMatcher(['/', '/api/webhooks(.*)']);
 
+// export default clerkMiddleware((auth, request) => {
+//   if (!isProtectedRoute(request)) {
+//     auth().protect()
+//   }
+// })
 
-export default clerkMiddleware((auth, request) => {
-  if (!isProtectedRoute(request)) {
-    auth().protect()
-  }
-})
-
-// export default clerkMiddleware();
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
