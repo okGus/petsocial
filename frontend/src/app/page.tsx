9a17129ec9fb8b35d6a5590bd056d3e4156a2316
Gustavo <rosalesagustavo@gmail.com>
2024-08-17 13:13:40 -0400

    clerk integration test aws

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,17 @@
 import UserPage from "../../component/UserPage";
+import { SignedIn, SignedOut, SignIn } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <UserPage />
+      <SignedOut>
+        <div className="flex justify-center p-5">
+          <SignIn routing="hash"/>
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <UserPage />
+      </SignedIn>
     </main>
   );
 }
